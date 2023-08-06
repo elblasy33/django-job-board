ad24bf85edf6b9f0a737831141ab78c8e0d0ffce
elblasy mohamed <elblasy@gmail.com>
2023-08-06 09:35:04 +0300

    add apps"

--- job/models.py
+++ job/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+JOP_TYPE=(
+    ('Full time','Full time'),
+    
+    ('Part time','Part time'),
+
+)
+class job(models.Model):
+    title=models.CharField(max_length=120)
+    #location
+    jop_type=models.CharField(max_length=15,choices=JOP_TYPE,default="Full time")
+    descraption=models.TextField(max_length=30,default=" ")
+    puplished_at=models.DateTimeField(auto_now=True,auto_now_add=False)
+    vacancy=models.IntegerField(default=1)
+    salary=models.IntegerField(default=0)
+    exprince=models.IntegerField(default=1)
+    
+    
+    def __str__(self):
+        return f'{self.title+"------------>"}{self.puplished_at}'
+    
+    
+ 
+
+    
+    
+    
